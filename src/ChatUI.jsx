556c700472ef9b6e1581381e57a55ddaeec3324e
Satish Kumar Sharma <skwebserv@gmail.com>
2025-02-27 13:43:44 +0530

    Disable button when processing

--- src/ChatUI.jsx
+++ src/ChatUI.jsx
@@ -127,13 +127,13 @@ const ChatUI = () => {
               placeholder="Ask something..."
               className="flex-grow focus:outline-none  p-6"
             />
-            <button type="submit" className="cursor-pointer ">
+            <button type="submit" className="cursor-pointer" disabled={loading}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                className="bi bi-arrow-up-circle-fill size-10"
+                className={`${loading && 'fill-gray-600 '} size-10`}
                 viewBox="0 0 16 16"
               >
                 <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
@@ -144,7 +144,7 @@ const ChatUI = () => {
 
         {error && <p className="text-red-500 mt-2">Error: {error}</p>}
       </div>
-    </div>
+    </div >
   );
 };
 
